f8cca2ced682691361c671e104412a215abb58bc
Sofchka <sofizaqar@gmail.com>
2026-06-01 21:12:08 +0400

    main corrected

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,11 +2,7 @@
 
 int main()
 {
-    Config config;
-    std::vector<int> clients;
-    if (config.load("conf/default.conf") == false)
-    {
-        std::cerr << "Error: Could not load config file\n";
-        return 1;
-    }
+    Server Server;
+    Server.init();
+    Server.run();
 }
