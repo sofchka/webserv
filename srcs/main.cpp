51a2d5989efb88201f631f9387c21b2945020ca5
Sofchka <sofizaqar@gmail.com>
2026-06-02 21:36:45 +0400

    fix: main cpp logic fixed(with input file)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,8 +1,20 @@
 #include "../includes/Server.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    std::string config_path;
+
+    if (argc > 2)
+    {
+        std::cerr << "Usage: ./webserv [configuration file(no mandatory)]" << std::endl;
+        return 1;
+    }
+    if (argc == 2)
+        config_path = argv[1];
+    else
+        config_path = "conf/default.conf";
+
     Server Server;
-    Server.init();
+    Server.init(config_path);
     Server.run();
 }
